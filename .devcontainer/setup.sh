1281b450fdd004e2cb762cf4f10e609d8b187442
Julio <gonzalez.julio8959@gmail.com>
2024-01-11 02:01:45 +0000

    wip devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+main(){
+    # TODO: setup everything for using kemu.
+    echo "TODO .devcontainer/setup.sh"
+    # dd if=/dev/zero of=qemu_disk count=1G status=progress
+    # cargo build ?
+}
+
+main "$@"
