9d4f6190446cdec69c4147d6ad36b98faea7460e
Julio <gonzalez.julio8959@gmail.com>
2024-01-11 02:12:59 +0000

    still wip

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,7 @@ main(){
     # TODO: setup everything for using kemu.
     echo "TODO .devcontainer/setup.sh"
     # dd if=/dev/zero of=qemu_disk count=1G status=progress
+    # dd if=/dev/zero of=qemu_disk bs=1 count=0 seek=1G status=progress
     # cargo build ?
 }
 
